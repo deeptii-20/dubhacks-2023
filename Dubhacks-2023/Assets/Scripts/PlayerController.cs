ec89c53c541313c11554bff4bdd2e0b1065e2c3d
Ashley Luty <aluty02@cs.washington.edu>
2023-10-14 14:28:57 -0700

    added pot prefab, fixed some controlelrs tuff

--- Dubhacks-2023/Assets/Scripts/PlayerController.cs
+++ Dubhacks-2023/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5.0f; // Adjust the movement speed as needed.
     private Rigidbody2D rb;
-    private Vector2 moveInput;
+
+    // key mappings
+    private KeyCode DRAG_KEY = KeyCode.R;
+
+    // State variables
+    private bool isDragging;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        isDragging = false;
     }
 
     private void Update()
@@ -20,18 +27,30 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        UpdateMove();
+    }
+
+    private void UpdateMove()
+    {
+        Vector2 moveInput;
+
         // Get input from the player (arrow keys or WASD)
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
-
-        Debug.Log("h: " + Input.GetAxisRaw("Horizontal"));
-        Debug.Log(Input.GetAxisRaw("Vertical"));
-
 
         // Normalize the input vector to ensure equal movement speed in all directions
         moveInput.Normalize();
 
         // Apply velocity to the Rigidbody to move the player
         rb.velocity = moveInput * moveSpeed;
+    }
+
+    private void UpdateDrag()
+    {
+        if (Input.GetKeyDown(DRAG_KEY))
+        {
+            // TODO: Check if we're in front of something
+            isDragging = !isDragging;
+        }
     }
 }
