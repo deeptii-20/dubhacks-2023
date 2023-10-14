2a256b6dc56887cf984e0b3fe659a5a66a30c9ca
Ashley Luty <aluty02@cs.washington.edu>
2023-10-14 15:04:05 -0700

    isdragging

--- Dubhacks-2023/Assets/Scripts/PlayerController.cs
+++ Dubhacks-2023/Assets/Scripts/PlayerController.cs
@@ -6,22 +6,28 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5.0f; // Adjust the movement speed as needed.
     private Rigidbody2D rb;
+    private Transform tr;
 
+    private Vector2 facingDirection;
 
-    // State variables
+    // Dragging variables
+    private static float DRAG_RADIUS = 1.0f;
+    private static LayerMask DRAG_MASK;
     private bool isDragging;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        tr = GetComponent<Transform>();
 
+        DRAG_MASK = LayerMask.GetMask("Moveable");
         isDragging = false;
+        facingDirection = new Vector2(0, -1);
     }
 
     private void Update()
     {
         UpdateDrag();
-
     }
 
     private void FixedUpdate()
@@ -37,6 +43,11 @@ public class PlayerController : MonoBehaviour
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
 
+        if (moveInput.magnitude > 0)
+        {
+            facingDirection = moveInput;
+        }
+
         // Normalize the input vector to ensure equal movement speed in all directions
         moveInput.Normalize();
 
@@ -50,7 +61,23 @@ public class PlayerController : MonoBehaviour
         {
             // TODO: Check if we're in front of something
             Debug.Log("Drag");
+            Debug.Log(facingDirection);
             isDragging = !isDragging;
+            IsFacingMoveableObject();
+        }
+    }
+
+    private void IsFacingMoveableObject()
+    {
+        Vector2 origin = tr.position;
+        // Cast a ray in the facing direction.
+        RaycastHit2D hit = Physics2D.Raycast(origin, facingDirection, DRAG_RADIUS, DRAG_MASK);
+        // Check if the ray hits something.
+        if (hit.collider != null)
+        {
+            Debug.Log("Ray hit: " + hit.collider.gameObject.name);
+
+            // You can perform actions or interactions with the object that was hit.
         }
     }
 }
